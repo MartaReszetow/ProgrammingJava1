3d18b38e9fd5a2e239deec998cfa3dae29399c8d
MARTA RESZETOW <marta.reszetow@gmail.com>
2020-02-01 10:34:27 +0100

    Christmas Calculator added

---
+++ src/main/java/pl/sda/MR/day4/AppMain.java
@@ -0,0 +1,12 @@
+package pl.sda.MR.day4;
+
+import java.time.LocalDate;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+
+
+        TimeCalculator.daysToCHristmasEve();
+    }
+}
---
+++ src/main/java/pl/sda/MR/day4/TimeCalculator.java
@@ -0,0 +1,39 @@
+package pl.sda.MR.day4;
+
+import java.time.LocalDate;
+
+//Stwórz klasę TimeCalculator. Stwórz w niej metodę daysToChristmasEveCalculator() wyświetlają informację w konsoli z komunikatem:
+// "It's just [ilość dni]to Christmas Eve!". Jeżeli pozostało mniej jak 60 dni dodaj w nowej linii "Time to buy gifts".
+// Metoda ta niech woła kolejną metode daysToChristmasEve(LocalDate now) do której przekażemy aktualną datę na podstawie
+// której zwróci wartość liczbową ilości dni pozostałych do najbliższej Wigilii świąt Bożego Narodzenia.
+// Spraw aby metoda była uniwersalna - jak najbliższe święta miną, powinna nadal działać. Pamiętaj również o końcówce roku.
+// Użyj klasy LocalDate i jej metody now(). Dwie różne daty możesz porównać poprzez ChronoUnit.DAYS.between().
+// Przetestuj też z metody main samą metodę daysToChristmasEve przekazując jej różne daty i drukując wynik.
+public class TimeCalculator {
+
+    // method that compares today with next
+    public static void daysToChristmasEveCalculator(LocalDate today) {
+
+        LocalDate dayOfChristmasEve = LocalDate.of(today.getYear(), 12, 24);  // date of Christmas Eve for current year
+        int daysToChristmas = dayOfChristmasEve.getDayOfYear() - today.getDayOfYear();
+
+        if (daysToChristmas<0){
+            LocalDate futureChristmas = LocalDate.of((today.getYear()+1),12,24);
+            LocalDate NewYearsDate = LocalDate.of(today.getYear(),12,31);
+            daysToChristmas = NewYearsDate.getDayOfYear()- today.getDayOfYear() + futureChristmas.getDayOfYear() +1;
+        }
+
+        System.out.println("It's just " + daysToChristmas + " to Christmas Eve");
+        if (daysToChristmas < 60) {
+            System.out.println("Time to but gifts!");
+        }
+    }
+
+
+
+    public static void daysToCHristmasEve() {
+    //    LocalDate today = LocalDate.now();
+        daysToChristmasEveCalculator(LocalDate.of(2018,12,25));
+            }
+
+}
